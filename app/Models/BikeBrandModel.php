5ba7b0213a1b760217f8d15261152d6dc8cff9ef
Mia <alice79101@gmail.com>
2023-05-19 14:49:28 +0800

    feat: bike_brands migration & seeder

--- app/Models/BikeBrandModel.php
+++ app/Models/BikeBrandModel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BikeBrandModel extends Model
 {
     use HasFactory;
+
+//    protected $fillable = [
+//        'name',
+//        'description',
+//    ];
 }
---
+++ database/migrations/2023_05_19_062209_create_bike_brands_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bike_brands', function (Blueprint $table) {
+            $table->id()->unique()->autoIncrement();
+            $table->integer('order')->default(0);
+            $table->string('name');
+            $table->string('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bike_brands');
+    }
+};
