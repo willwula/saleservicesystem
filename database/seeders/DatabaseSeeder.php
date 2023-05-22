1bff5f4560180d6fe7e3d9c056f916da814f3f8f
tsao <tsaoyuan@gmail.com>
2023-05-22 19:45:38 +0800

    feat: product migration & seeder
    - purchase_proof 欄位資料，由 Morph Image 的動態關聯產生 (未完成)
    - 日期格式化，用 laravel 內建 `Carbon` class
    - `product_number`，Product 模型中取得最新的資料記錄的 ID + 1
      - `latest('id')` 用於按照 ID 欄位的降序排列資料，即最新的資料排在前面
      - `first()` 用於取得排序後的第一筆資料，即最新的資料
      - `id` 取得該資料的 ID 值，為一數字
      - `+ 1` 新資料的 ID 值加上 1，得到下一個可用的 `product_number`
    - `str_pad()` 用來灌資料，將字串填充（padding）到指定的長度，例如 product_number = 1，則輸出 6位數字: `00000`1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Product;
-use App\Models\BikeModel;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
