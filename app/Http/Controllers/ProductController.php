47cdb299e13869d66a87363872f7d67e3881b7b1
tsao <tsaoyuan@gmail.com>
2023-05-22 13:12:21 +0800

    feat: Create Product controller & route(index, store)
    - Product of Route use apiResource

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return 'index';
+    }
+
+    public function store(){
+        return 'store';
+    }
+}
