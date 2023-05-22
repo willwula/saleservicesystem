33490eea0aee0773a838941e59b0174915dfccde
Mia <alice79101@gmail.com>
2023-05-22 15:17:51 +0800

    fix: BikeBrandResource array 排版

--- app/Http/Resources/BikeBrandResource.php
+++ app/Http/Resources/BikeBrandResource.php
@@ -9,15 +9,15 @@ class BikeBrandResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'order' => $this->order,
-            'name' => $this->name,
+            'id'          => $this->id,
+            'order'       => $this->order,
+            'name'        => $this->name,
             'description' => $this->description,
         ];
     }
