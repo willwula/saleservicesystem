7c17da97ae1666d3efdd5d442d8170a585d2bcbb
tsao <tsaoyuan@gmail.com>
2023-05-22 19:45:38 +0800

    feat: product migration & seeder
    - purchase_proof 欄位資料，由 Morph Image 的動態關聯產生 (未完成)
    - 日期格式化，用 laravel 內建 `Carbon` class
    - `product_number`，Product 模型中取得最新的資料記錄的 ID + 1
      - `latest('id')` 用於按照 ID 欄位的降序排列資料，即最新的資料排在前面
      - `first()` 用於取得排序後的第一筆資料，即最新的資料
      - `id` 取得該資料的 ID 值，為一數字
      - `+ 1` 新資料的 ID 值加上 1，得到下一個可用的 `product_number`
    - `str_pad()` 用來灌資料，將字串填充（padding）到指定的長度，例如 product_number = 1，則輸出 6位數字: `00000`1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,6 +24,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             BikeBrandsSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Product $product)
+    {
+        $number = $product->latest('id')->first()->id + 1;
+        $numberOfDigits = 6;
+        $formattedDate = Carbon::now()->format('ymd');
+        $registedDate = Carbon::now()->format('Y-m-d'); 
+        DB::table('products')->insert([
+            'customer_id' => fake()->numberBetween(1, 3),
+            'product_number' => $formattedDate . str_pad($number, $numberOfDigits, '0', STR_PAD_LEFT),
+            'serial_number' => fake()->numberBetween(1, 5),
+            'bike_brand_name' => fake()->randomElement(['Giant', 'Merida', 'Trek', 'Specialized', 'Cannondale', 'Bianchi']),
+            'registed_date' => $registedDate,
+            'bike_model_name' => fake()->randomElement(['Off-Road', 'Enduro']),
+            'mortor_barcode' => Str::random(6),
+            'created_at' => now(),
+        ]);
+    }
+}
