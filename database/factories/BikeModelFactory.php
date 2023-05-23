7516e6169e116fdfd6cc973c6c8bdce81f1033b7
Mia <alice79101@gmail.com>
2023-05-23 16:22:45 +0800

    fix: 修改BikeModelFactory 命名

--- database/factories/BikeModelFactory.php
+++ database/factories/BikeModelFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BikeModel>
  */
-class bikeModelFactory extends Factory
+class BikeModelFactory extends Factory
 {
     /**
      * Define the model's default state.
