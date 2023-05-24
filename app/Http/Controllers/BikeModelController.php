7e5c4d259d40d56802e431eecdb0428797701692
Mia <alice79101@gmail.com>
2023-05-24 16:09:46 +0800

    feat: bike_model show & store

--- app/Http/Controllers/BikeModelController.php
+++ app/Http/Controllers/BikeModelController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BikeBrandResource;
 use App\Http\Resources\BikeModelCollection;
+use App\Http\Resources\BikeModelResource;
 use App\Models\BikeBrand;
 use App\Models\BikeModel;
 use Illuminate\Http\Request;
@@ -47,5 +49,37 @@ class BikeModelController extends Controller
         }
 
         return BikeModelCollection::make($bikeModels->get());
+    }
+
+    /**
+     * 查看單一 bike_model 內容
+     *
+     * @urlParam id 車款id Example: 1
+     */
+    public function show(BikeModel $bikeModel)
+    {
+        return BikeModelResource::make($bikeModel);
+    }
+
+    /**
+     * 新增 bike_model
+     *
+     * @bodyparam bike_brand_id 所屬廠牌id Example: 1
+     * @bodyparam name string:255 車款名稱 Example: 登山車
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'bike_brand_id' => 'int|required',
+            'name' => 'string|required|max:255',
+        ]);
+
+        abort_if(
+            BikeBrand::find($validated['bike_brand_id']) === null,
+            Response::HTTP_BAD_REQUEST,
+            __('無此廠牌，請重新確認')
+        );
+
+        return BikeModelResource::make(BikeModel::create($validated));
     }
 }
